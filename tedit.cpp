56a1d1fb840f94eb4745e91a4f4999bfc7f147b9
Aseem Yash <33717548+asymysh@users.noreply.github.com>
2023-06-25 19:00:45 +0530

    Final Class and Sample Main

--- tedit.cpp
+++ tedit.cpp
@@ -21,7 +21,6 @@ public:
 
 
 //Insert Text using String Library
-  
     void addT(string str){                            //Function call
         this->text.insert(cursor, str);                //Insert at cursor position
         cursor += str.length();                        //Update cursor value
@@ -29,7 +28,6 @@ public:
 
 
 //Delete k Letters from the Left
-
     void deleteT(int k){                                //Function call        
         if (k>cursor){                                  //Special condition check
             text.erase(0, cursor);                        //Remove all in special condition
@@ -43,31 +41,28 @@ public:
 
 
 //Delete 1 Letters from the Left
-
     void backspace(){                                   //Function call
         deleteT(1);                                     //??
     }                                                   
 
 
 //Find Cursor
-
     int findCursor(){                                    //Function call
         return cursor;                                   //Found cursor
     }
 
 
-
 //Move Cursor to the Left/Right
-    void cursorLeft(int k) {
-        if(k > cursor)
+    void cursorLeft(int k) {                            //Function Call
+        if(k > cursor)                                  //Edge case
             {
-                cursor = 0; 
-                return;
+                cursor = 0;                             //Handle the exception :O
+                return;                                 //Retun    
             }
-        cursor = cursor-k;
+        cursor = cursor-k;                              //General condition
     }
 
-    void cursorRight(int k) {
+    void cursorRight(int k) {                            //Same as above for the opposite end
         if(k+cursor > text.length())
             {
                 cursor=text.length();
@@ -78,10 +73,10 @@ public:
 
 
 
-//Display the Full Text
+//Display the Full Text                                
     string fullText(){
-        cout<<text<<endl;
-        return text;
+        cout<<text<<endl;                            //Display
+        return text;                                 //And return since thats what the given function signature demands
     }
 
 
@@ -91,7 +86,7 @@ public:
         if( k+cursor > text.length())                           //Check for exceptional case
             k = text.length() - cursor;                         //Update value of k with number of chars on the right that CAN be displayed
 
-        cout<<text.substr(cursor, k);
+        cout<<text.substr(cursor, k)<<endl;
         return text.substr(cursor, k);
         
     }
@@ -118,6 +113,10 @@ int main() {
     
     editor.fullText();
 
+    editor.deleteT(3);
+    editor.fullText();
+
+    editor.cursorLeft(4);
     editor.showText(4);
 
     return 0;
