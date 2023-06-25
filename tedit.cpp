b441b0b95b691f8cb310d80fef4012f8b43ccc1a
Aseem Yash <33717548+asymysh@users.noreply.github.com>
2023-06-25 17:37:11 +0530

    Added DeleteT

--- tedit.cpp
+++ tedit.cpp
@@ -22,10 +22,10 @@ Done
 
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class TextEditor {
+
+class TextEdito{
 
 protected:
     string text;
@@ -43,13 +43,26 @@ public:
 
 //Insert Text using String Library
   
-    void addText(string& text) {                        //Function call
+    void addT(string& text){                            //Function call
         this->text.insert(cursor, text);                //Insert at cursor position
         cursor += text.length();                        //Update cursor value
     }
 
 
 //Delete k Letters from the Left
+
+    void deleteT(int k){                                //Function call
+        
+        if (k>cursor){                                  //Special condition check
+            text.erase(0, cursor);                        //Remove all in special condition
+            cursor = 0;                                   //Reset cursor in special condition
+            } 
+        
+        text.erase(cursor - k, k);                        //Remove data from cursor position minus K to cursor position
+        cursor -= k;                                      //Update cursor position
+    }
+
+
 
 
 
