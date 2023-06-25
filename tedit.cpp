b130045cf9bdea16fe47f229249ba8fc93756b8c
Aseem Yash <33717548+asymysh@users.noreply.github.com>
2023-06-25 18:54:38 +0530

    Added main

--- tedit.cpp
+++ tedit.cpp
@@ -1,31 +1,10 @@
-// Implement the TextEditor class:
-Semi done
-// TextEditor() Initializes the object with empty text.
-Done
-// void addText(string text) Appends text to where the cursor is. The cursor ends to the right of text.
-Done
-// void deleteText(int k) Deletes k characters to the left of the cursor.
-Done
-// void backspace() Deletes 1 character to the left of the cursor.
-Done
-// int findCursor() Returns the index position of the cursor.
-Done
-// void cursorLeft(int k) Moves the cursor to the left k times. Equivalent to pressing the left arrow key on a keyboard
-Done
-// void cursorRight(int k) Moves the cursor to the right k times. Equivalent to pressing the right arrow key on a keyboard.
-Done
-// string fullText() Returns all the text from the start to the finish.
-Done
-// string showText(int k) Returns the next k characters to the right of the cursor.
-Done
-
 
 #include <iostream>
 #include <string>
 using namespace std;
 
 
-class TextEdito{
+class TextEditor{
 
 protected:
     string text;
@@ -43,7 +22,7 @@ public:
 
 //Insert Text using String Library
   
-    void addT(string& str){                            //Function call
+    void addT(string str){                            //Function call
         this->text.insert(cursor, str);                //Insert at cursor position
         cursor += str.length();                        //Update cursor value
     }
@@ -51,11 +30,11 @@ public:
 
 //Delete k Letters from the Left
 
-    void deleteT(int k){                                //Function call
-        
+    void deleteT(int k){                                //Function call        
         if (k>cursor){                                  //Special condition check
             text.erase(0, cursor);                        //Remove all in special condition
             cursor = 0;                                   //Reset cursor in special condition
+            return;
             } 
         
         text.erase(cursor - k, k);                        //Remove data from cursor position minus K to cursor position
@@ -81,13 +60,19 @@ public:
 //Move Cursor to the Left/Right
     void cursorLeft(int k) {
         if(k > cursor)
-            cursor = 0;
+            {
+                cursor = 0; 
+                return;
+            }
         cursor = cursor-k;
     }
 
     void cursorRight(int k) {
         if(k+cursor > text.length())
-            cursor=text.length();
+            {
+                cursor=text.length();
+                return;
+            }
         cursor=cursor+k;
    }
 
@@ -95,7 +80,7 @@ public:
 
 //Display the Full Text
     string fullText(){
-        cout<<text;
+        cout<<text<<endl;
         return text;
     }
 
@@ -114,7 +99,28 @@ public:
 
 
 
+int main() {
+    
+    TextEditor editor;
+    
+    editor.addT("Hello World");
+    editor.fullText();
+    
+    editor.backspace();
+    editor.fullText();
 
+    editor.cursorLeft(2);
+    editor.deleteT(3);
+    editor.fullText();
 
+    editor.addT("Bellow");
+    editor.cursorRight(3);
+    
+    editor.fullText();
+
+    editor.showText(4);
+
+    return 0;
 }
+
 
